4182b4d374428e65fbe4d079ab3d10e666e603a2
maiteduron <57925490+maiteduron@users.noreply.github.com>
2020-03-10 19:45:59 -0600

    Add files via upload

---
+++ APP/js/my-app.js
@@ -0,0 +1,40 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'App Coffee', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/coffee/',
+    	url: 'coffee.html',
+    	name: 'coffee',
+  		},
+		{
+		path: '/drinks/',
+    	url: 'drinks.html',
+    	name: 'drinks',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
